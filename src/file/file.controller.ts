08fe9f31e69d7a62f63eca41be4051c7d4df19a9
LanselonX <101521725+LanselonX@users.noreply.github.com>
2026-01-02 15:51:33 +0200

    fix: update file

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -3,13 +3,20 @@ import {
   HttpStatus,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Role } from 'generated/prisma/enums';
 import { diskStorage } from 'multer';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { editFileName, imageFileFilter } from 'src/utils/file-utils';
 
 @Controller('upload')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(Role.ADMIN)
 export class FileController {
   @Post()
   @UseInterceptors(
